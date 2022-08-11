a89b9c04c1faec19b822ba2319fae82f004b368b
fabiovmat <fabio.meihua08@gmail.com>
2022-08-11 00:36:22 -0300

    form update

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,11 +1,11 @@
-import { SharedModule } from './../shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from './../shared/shared.module';
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { CourseFormComponent } from './course-form/course-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
